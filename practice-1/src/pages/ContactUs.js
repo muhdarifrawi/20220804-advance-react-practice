ffb9690e6534317580761a8e154196cf762edbc1
arif <muhdarifrawi@gmail.com>
2022-08-04 14:03:29 +0800

    added into ContactUs.js

--- practice-1/src/pages/ContactUs.js
+++ practice-1/src/pages/ContactUs.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ContactUs(){
+    
+    const navigate = useNavigate()
+
+    function submitForm(){
+        navigate("/form-submitted")
+    }
     return(
         <React.Fragment>
             <h1>Contact Us</h1>
+            <div>
+                <div>
+                    <label>Full Name:</label>
+                    <input type="text" name="fullname"/>
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input type="text" name="email"/>
+                </div>
+                <button onClick={submitForm}>Submit</button>
+            </div>
         </React.Fragment>
     )
 }
