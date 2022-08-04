25112aa018f121bf512b1f6d7e13a6500e23ca59
arif <muhdarifrawi@gmail.com>
2022-08-04 11:17:06 +0800

    Created router and routes for components.

--- practice-1/src/App.js
+++ practice-1/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React from 'react';
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -9,7 +9,31 @@ import ContactUs from './pages/ContactUs';
 function App() {
   return (
     <React.Fragment>
-
+      {/* Router `points` the link to the Routes */}
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Routes is where the pages will appear */}
+        {/* Routes should be nested inside the Router */}
+        {/* path should match with `to` in the Router/Link */}
+        {/* element points to the imported component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Router>
     </React.Fragment>
   );
 }
