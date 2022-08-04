9bc5179b1771b0ce5b41cbb3d15596fe7f0fc029
arif <muhdarifrawi@gmail.com>
2022-08-04 10:50:38 +0800

    practice-1 initial commit.

---
+++ practice-1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, link} from "react-router-dom";
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
