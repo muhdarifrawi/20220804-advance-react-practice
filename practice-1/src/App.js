0b3de72f90c2e3b4540f96793c6329a345f9a296
arif <muhdarifrawi@gmail.com>
2022-08-04 10:55:57 +0800

    Creates Pages folder: - Created components: About.js, ContectUs.js and Home.js - Import them to App.js

--- practice-1/src/App.js
+++ practice-1/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, link} from "react-router-dom";
 import React from 'react';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <React.Fragment>
-      
+
     </React.Fragment>
   );
 }
